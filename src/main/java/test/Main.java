3affece2dbe506f879f6494f35a92f68382023d4
robert33333 <robert.banu18@gmail.com>
2018-08-23 17:38:22 +0300

    Second commit.

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -5,9 +5,7 @@ import data.otherStructures.Parameter;
 
 import java.util.ArrayList;
 
-import static controller.Controller.addFunction;
-import static controller.Controller.printAll;
-import static controller.Controller.startTrace;
+import static controller.Controller.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,6 +14,7 @@ public class Main {
         startTrace(new Function("main",parameters));
 
         a("testString");
+        d("unu", "doi");
     }
 
     public static void a(String string) {
@@ -31,6 +30,13 @@ public class Main {
         parameters.add(new Parameter("int1",int1));
         parameters.add(new Parameter("int2",int2));
         addFunction("main","a",new Function("b",parameters));
+    }
+
+    public static void d(String a, String b) {
+        ArrayList<Parameter> parameters = new ArrayList<>();
+        parameters.add(new Parameter("a", a));
+        parameters.add(new Parameter("b", b));
+        addFunction("main", "main", new Function("d", parameters));
 
         printAll("main");
     }
